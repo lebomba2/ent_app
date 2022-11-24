b238790a2af444fe99ed1785fa1527edb60fe7c8
Chris Lebombard <lebomba2@gmail.com>
2022-11-23 20:50:39 -0600

    Added Occupation Class

---
+++ Entertainment_App/Models/Occupation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertainment_App.Models
+{
+    class Occupation
+    {
+
+        public String Name { get; set; }
+        public Occupation()
+        {
+            Name = "unknown";
+        }
+
+        public void AddName() {
+
+            Console.Write("Enter occupation name: ");
+            this.Name = Console.ReadLine();
+        }
+    }
+}
