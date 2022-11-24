3fc62b97861d3911e802005e3efdcbb8d41f35a7
Chris Lebombard <lebomba2@gmail.com>
2022-11-23 18:48:38 -0600

    Reorganized the structure of the solution

---
+++ Entertainment_App/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entertainment_App.Models
+{
+    class User
+    {
+        //public Var Age;
+        //public varGender;
+        //public        var ZipCode;
+
+    }
+}
